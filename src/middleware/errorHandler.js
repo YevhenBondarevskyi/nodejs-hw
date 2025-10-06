63a9fd3f372203febfb5e9c3bd0734be40a73c2b
Yevhen <bondarevskiy@gmail.com>
2025-10-06 16:28:50 +0200

    hw2nj

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,15 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (error, req, res, next) => {
+  if (error instanceof HttpError) {
+    return res.status(error.status).json({
+      message: error.message || error.name,
+    });
+  }
+
+const isProd = process.env.NODE_ENV === 'production';
+  console.error(error);
+  res.status(500).json({
+    message: isProd ? "Something went wrong!" : error.message,
+  });
+};
